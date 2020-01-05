8f65a307b1eed93179c0661bdc0aa3c17179ee55
dejoM <dejo919@outlook.com>
2020-01-05 12:24:02 +0100

    Brisanje eventa(sa updateom na index)

--- Osi_Project_group14/Functions/DataManage.h
+++ Osi_Project_group14/Functions/DataManage.h
@@ -1,10 +1,4 @@
 #pragma once
-/*OK, prije svega ovo su samo osnovne funkije za upis i brisanje unutaj fajla. Jos treba napisati funkcija da kod bude pregledniji i tako to.
-Sto se tice fajlova, zasad samo podrazumjevani su 'Users' i 'Events' (u folderu 'Data'), kao glavni gdje se cuvaju podaci.
-Format u kojem su upisani i u kojem se citajuse moze vidjeti iz funkcija; a mislio sam da na pocetku fajla bude kao broj na osnovu
-kojeg se racuna ID i za korinike i dogadjaje, naravno posto je to string mogu biti i slova, ali onda se mora dodatno definisati dodjeljjivanje id-a, naravno to se moze izbaciti.
-Funkcije za upis/ispis su samo tu radi formata, pa jos treba da se napisu one koje provjeravaju postojece korisnike i dogadjaje ali ima vremena.
-*/
 #include <stdio.h>
 
 
