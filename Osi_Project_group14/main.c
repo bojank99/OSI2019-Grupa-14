5b56852ca8b35ce9b940de609579329d06d0ee41
dejoM <dejo919@outlook.com>
2020-01-05 13:53:51 +0100

    main izmjena

--- Osi_Project_group14/main.c
+++ Osi_Project_group14/main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Functions/functions.h"
-#include "Functions/DataManage.h"
+
 
 
 int main()
 {
-    FILE* unUserFile;
-    UNREGUSER* temp=(UNREGUSER*)calloc(1,sizeof(UNREGUSER));
-    allocUnregUser(temp);
-    openUnregUserData(&unUserFile,"r+");
-    loadUnregUser(temp,unUserFile);
-    printf("%s %s %s %s %s",temp->base.userName,temp->base.password,
-           temp->email,temp->name,temp->surname);
+    char cityName[20];
+
+    isThereCity(cityName);
+
+    chooseOption(cityName);
+
     return 0;
 
 }
